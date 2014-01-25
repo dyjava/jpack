8eabf05d42e41bcec91b54fe70e0d2fe3677de3b
diyong <dyong525@163.com>
2014-01-25 17:03:41 +0800

    xpath

---
+++ src/com/my/parser/XPath.java
@@ -0,0 +1,84 @@
+package com.my.parser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+import org.htmlcleaner.XPatherException;
+
+/**
+ * made by dyong 
+ * 
+ **/
+public class XPath {
+	private HtmlCleaner cleaner = new HtmlCleaner();
+	private static XPath xpath = new XPath() ;
+	private TagNode rootNode ;
+	
+	public static XPath getInstens(String content){
+		try {
+			xpath.rootNode = xpath.cleaner.clean(content);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return xpath ;
+	}
+
+	public List<String> getHref(String content,String xpath) throws IOException, XPatherException{
+//		TagNode rootNode = cleaner.clean(content);
+
+		Object[] tns = rootNode.evaluateXPath(xpath) ;
+		if(tns==null || tns.length==0){
+			return new ArrayList<String>() ;
+		}
+		List<String> list = new ArrayList<String>() ;
+		for(Object obj:tns){
+			String msg = ((TagNode)obj).getAttributeByName("href");
+			list.add(msg) ;
+		}
+		return list;
+		
+	}
+	
+
+	public List<String> getHtml(String content,String xpath) throws IOException, XPatherException{
+//		TagNode rootNode = cleaner.clean(content);
+
+		Object[] tns = rootNode.evaluateXPath(xpath) ;
+		if(tns==null || tns.length==0){
+			return new ArrayList<String>() ;
+		}
+		List<String> list = new ArrayList<String>() ;
+		for(Object obj:tns){
+			String msg = cleaner.getInnerHtml((TagNode)obj);
+			msg = msg.replaceAll("<.*?>", "").trim() ;
+			list.add(msg) ;
+		}
+		return list;
+		
+	}
+	
+	public HashMap<String,List<String>> getHtml(String content,List<String> xpath) throws IOException, XPatherException{
+//		TagNode rootNode = cleaner.clean(content);
+//		String ss = cleaner.getInnerHtml((TagNode)rootNode.evaluateXPath("//div[@id='htmlContent']")[0]) ;
+		HashMap<String,List<String>> result = new HashMap<String,List<String>>() ;
+		for(String item:xpath){
+			Object[] tns = rootNode.evaluateXPath(item) ;
+			if(tns==null || tns.length==0){
+				result.put(item,new ArrayList<String>()) ;
+			}
+			List<String> list = new ArrayList<String>() ;
+			for(Object obj:tns){
+				String msg = cleaner.getInnerHtml((TagNode)obj);
+				list.add(msg) ;
+			}
+			result.put(item, list) ;
+		}
+		return result;
+		
+	}
+}
---
+++ test/com/my/test/parser/XPathTest.java
@@ -0,0 +1,61 @@
+package com.my.test.parser;
+
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.my.file.WriteFile;
+import com.my.file.web.MyHttpClient;
+import com.my.parser.XPath;
+
+public class XPathTest {
+
+	@Test
+	public void test() {
+		String url = "http://www.lz13.cn/ganenlizhi/20296.html" ;
+		url = "http://www.lz13.cn/ganenlizhi/8290.html" ;
+		String path = "e:/img/txt/" ;
+		
+		int i=0 ;
+		while(url.trim().length()>0){
+			i++ ;
+			if(i==100){
+				return ;
+			}
+			String content = MyHttpClient.getInstance().getUrlContent(url,"utf-8",false) ;
+			XPath test = XPath.getInstens(content) ;
+			
+			try{
+				String xp = ".//*[@id='node-8890']/div[2]/p[1]/strong" ;
+				xp = ".//*[@id='node-8890']/div[1]/h2" ;
+				List<String> list = test.getHtml(content,xp) ;
+				String title = list.get(0).trim() ;
+				System.out.println(title) ;
+				String filePath = path + title+".txt" ;
+				
+				System.out.println("=============1===========") ;
+				list = test.getHtml(content,".//*[@id='node-8890']/div[2]/p") ;
+				for(String item:list){
+					System.out.println(item) ;
+				}
+				WriteFile.StringWriteToFile(filePath, list.toArray(new String[0])) ;
+				
+		
+				System.out.println("==========2=============") ;
+				list = test.getHref(content,".//*[@id='node-8890']/div[2]/div[6]/a[2]") ;
+				url = list.get(0).trim() ;
+				System.out.println(url) ;
+			} catch (Exception e){
+//				e.printStackTrace() ;
+				System.out.println("ERROR:"+e.getMessage()) ;
+			}
+			try {
+				Thread.sleep(1*1000) ;
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+}
