91dcea31a9ae36f50dda35c75ec9d737b64f1c5a
dyong <dyong525@163.com>
2014-07-15 09:30:31 +0800

    IP获取

---
+++ src/com/my/util/IP.java
@@ -0,0 +1,30 @@
+package com.my.util;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Formatter;
+import java.util.Locale;
+
+public class IP {
+
+	public static void main(String[] args) throws Exception{
+		IP.ipMac() ;
+	}
+    private static void ipMac() throws Exception{
+        InetAddress address = InetAddress.getLocalHost();
+        NetworkInterface ni = NetworkInterface.getByInetAddress(address);
+        ni.getInetAddresses().nextElement().getAddress();
+        byte[] mac = ni.getHardwareAddress();
+        String sIP = address.getHostName()+address.getHostAddress();
+        String sMAC = "";
+        Formatter formatter = new Formatter();
+        for (int i = 0; i < mac.length; i++) {
+            sMAC = formatter.format(Locale.getDefault(), "%02X%s", mac[i],
+                    (i < mac.length - 1) ? "-" : "").toString();
+  
+        }
+        System.out.println(sIP) ;
+        System.out.println(sMAC) ;
+        
+    }  
+}
