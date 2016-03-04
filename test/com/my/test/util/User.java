4fd0b029360dc8eacfd0ad9e82d5347c30096ee0
dyong <dyong525@163.com>
2016-03-04 12:09:07 +0800

    d

--- test/com/my/test/util/User.java
+++ test/com/my/test/util/User.java
@@ -4,12 +4,17 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 //json解析对象，必须是public的对象
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "user") 
 public class User {
 	
+	@SerializedName("name")
 	public String name ;
+	@Expose(serialize=false)
 	public int age ;
 	public String getName() {
 		return name;
