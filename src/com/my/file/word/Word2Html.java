6f0d143b14e7c9fc47f7efb5ec123a54dbf18ff7
dyong <dyong525@163.com>
2016-03-28 16:41:41 +0800

    word

---
+++ src/com/my/file/word/Word2Html.java
@@ -0,0 +1,108 @@
+package com.my.file.word;
+
+import org.apache.poi.hwpf.HWPFDocument; 
+import org.apache.poi.hwpf.converter.PicturesManager; 
+import org.apache.poi.hwpf.converter.WordToHtmlConverter; 
+import org.apache.poi.hwpf.usermodel.Picture; 
+import org.apache.poi.hwpf.usermodel.PictureType; 
+import org.jsoup.Jsoup; 
+import org.w3c.dom.Document; 
+ 
+import javax.xml.parsers.DocumentBuilderFactory; 
+import javax.xml.parsers.ParserConfigurationException; 
+import javax.xml.transform.OutputKeys; 
+import javax.xml.transform.Transformer; 
+import javax.xml.transform.TransformerException; 
+import javax.xml.transform.TransformerFactory; 
+import javax.xml.transform.dom.DOMSource; 
+import javax.xml.transform.stream.StreamResult; 
+import java.io.*; 
+import java.util.List; 
+ 
+/** 
+ * 
+ */ 
+public class Word2Html { 
+ 
+	
+	public static void main(String argv[]) {
+		try {
+			convert2Html("e:\\test.doc","e:/data/11.html");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	//输出html文件 
+	public static void writeFile(String content, String path) {
+		FileOutputStream fos = null;
+		BufferedWriter bw = null;
+		org.jsoup.nodes.Document doc = Jsoup.parse(content);
+		content=doc.html();
+		try {
+			File file = new File(path);
+			fos = new FileOutputStream(file);
+			bw = new BufferedWriter(new OutputStreamWriter(fos,"UTF-8"));
+			bw.write(content);
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (bw != null)
+				bw.close();
+				if (fos != null)
+				fos.close();
+			} catch (IOException ie) {}
+		}
+	}
+	
+	//word 转 html 
+	public static void convert2Html(String fileName, String outPutFile) 
+			throws TransformerException, IOException, ParserConfigurationException {
+		FileInputStream stream = new FileInputStream(fileName) ;
+		HWPFDocument wordDocument = new HWPFDocument(stream);//WordToHtmlUtils.loadDoc(new FileInputStream(inputFile)); 
+		//兼容2007 以上版本 
+		// XSSFWorkbook xssfwork=new XSSFWorkbook(new FileInputStream(fileName));
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument() ;
+		WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(doc) ;
+		wordToHtmlConverter.setPicturesManager(new PicturesManager() {
+			public String savePicture( byte[] content, PictureType pictureType, String suggestedName,
+			float widthInches, float heightInches ) {
+				return "images/"+suggestedName ;
+			}
+		});
+		wordToHtmlConverter.processDocument(wordDocument) ;
+		//save pictures 
+		List<Picture> pics=wordDocument.getPicturesTable().getAllPictures() ;
+		if(pics!=null) {
+			for(int i=0;i<pics.size();i++){
+				Picture pic = pics.get(i);
+				System.out.println();
+				try {
+					File out = new File(outPutFile) ;
+					out = new File(out.getParent()+"/images/") ;
+					out.mkdir() ;
+					pic.writeImageContent(new FileOutputStream(out.getPath()+"/" + pic.suggestFullFileName()));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		Document htmlDocument = wordToHtmlConverter.getDocument();
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		DOMSource domSource = new DOMSource(htmlDocument);
+		StreamResult streamResult = new StreamResult(out);
+		
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer serializer = tf.newTransformer();
+		serializer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+		serializer.setOutputProperty(OutputKeys.METHOD, "HTML");
+		serializer.transform(domSource, streamResult);
+		out.close();
+		writeFile(new String(out.toByteArray()), outPutFile);
+	}
+}
